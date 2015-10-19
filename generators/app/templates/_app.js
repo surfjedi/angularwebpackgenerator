b0eb15bb1e01f4d889e00fd822181f2762bca9e5
breninho94 <breninho94@gmail.com>
2015-10-19 14:44:33 -0200

    fix erros template core

--- generators/app/templates/_app.js
+++ generators/app/templates/_app.js
@@ -1,9 +1,18 @@
 'use strict';
 
+let coreDirectives = require('core/core.directives');
+let coreFactories = require('core/core.factories');
+let coreProviders = require('core/core.providers');
+let coreServices  = require('core/core.services');
+
 export default require('angular')
     .module('<%= app_name %>', [
         require('angular-ui-router'),
         // @SEE: It's a hack! https://github.com/ocombe/ocLazyLoad/issues/179
         (() => { require('oclazyload'); return 'oc.lazyLoad' })(),
-        require('./pages/home/home.routing').name
+        require('./pages/home/home.routing').name,
+        coreDirectives.name,
+        coreFactories.name,
+        coreProviders.name,
+        coreServices.name
     ]);
--- generators/app/templates/core/_bootstrap.js
+++ generators/app/templates/core/_bootstrap.js
@@ -1,15 +1,7 @@
 'use strict';
 
 let appModule = require('../app');
-let coreDirectives = require('core.directives');
-let coreFactories = require('core.factories');
-let coreProviders = require('core.providers');
-let coreServices  = require('core.services');
 
 angular.bootstrap(document, [
-    appModule.name,
-    coreDirectives.name,
-    coreFactories.name,
-    coreProviders.name,
-    coreServices.name
+    appModule.name
 ], { strictDi: true });
